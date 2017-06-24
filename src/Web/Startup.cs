fbb17efe3ad23718f6ded3391c4b6bb73704b002
DeveTho <DeveTho@users.noreply.github.com>
2017-06-24 12:38:06 +0200

    Setup project startup and dependency injection

--- src/Web/Startup.cs
+++ src/Web/Startup.cs
@@ -1,34 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using MinuteOfHappiness.Frontend.Data.Context;
+using Serilog;
 
 namespace MinuteOfHappiness.Frontend.Web
 {
     public class Startup
     {
-        public void ConfigureServices(IServiceCollection services)
+        public Startup(IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            // Add the configuration settings
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddJsonFile("appsettings.ConnectionStrings.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+
+            // Add the logger to the pipeline
+            var serilog = new LoggerConfiguration()
+                .ReadFrom.Configuration(Configuration);
+
+            loggerFactory.AddSerilog(serilog.CreateLogger());
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-        {
-            loggerFactory.AddConsole();
+        public IConfigurationRoot Configuration { get; set; }
 
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services
+            services.AddMvc();
+
+            services.AddScoped<ApplicationDbContext>(factory => 
+                new ApplicationDbContext(Configuration.GetConnectionString("DefaultConnection")));
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            // Add the exception page when in development
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseMvc();
         }
     }
 }
